42a39be36750d33e9655bbd5cb1c8521ddf3843f
elley <a>
2023-02-28 10:01:05 +0900

    Gradle issue

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,6 +32,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void init(){
         navView = findViewById(R.id.nav_view);
         navView.setNavigationItemSelectedListener(this);
+
+        // Write a message to the database
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("table");
+
+        myRef.setValue("Hello, World!");
     }
 
     @Override
@@ -56,13 +71,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.id_sign_up:
+                signDialog(R.string.sign_up, R.string.sign_up_btn);
                 Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
-
                 break;
 
             case R.id.id_sign_in:
+                signDialog(R.string.sign_in, R.string.sign_in_btn);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
-
                 break;
 
             case R.id.id_sign_out:
@@ -71,5 +86,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return true;
+    }
+
+    private void signDialog(int title, int buttonTitle){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.layout_sign_up, null);
+
+        dialogBuilder.setView(dialogView);
+
+        TextView signTitle = dialogView.findViewById(R.id.tvSignTitle);
+        signTitle.setText(title);
+
+        Button signBtn = dialogView.findViewById(R.id.signButton);
+        signBtn.setText(buttonTitle);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+        if (title == R.string.sign_up){
+            signBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "sign up", Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
+        }else if(title == R.string.sign_in){
+            signBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "sign in", Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        }
     }
 }
